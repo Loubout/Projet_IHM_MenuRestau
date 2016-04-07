487787da1eb68d778bca3ee18d92c692e0a8962c
fyrthis <tanguy.retail@caramail.com>
2016-04-08 00:13:50 +0200

    maj food architecture, imp. Parcelable

--- Projet_IHM_android/app/src/main/java/com/example/willy/projet_ihm_android/data/food/ProductAbstract.java
+++ Projet_IHM_android/app/src/main/java/com/example/willy/projet_ihm_android/data/food/ProductAbstract.java
@@ -1,9 +1,57 @@
 package com.example.willy.projet_ihm_android.data.food;
 
+import android.os.Parcel;
+
 /**
- * Created by Willy on 07/04/2016.
+ * Created by tanguinoche on 07/04/2016.
  */
-public abstract class ProductAbstract extends com.example.willy.projet_ihm_android.data.food.ItemAbstract {
+public abstract class ProductAbstract extends ItemAbstract {
+
+    protected ProductAbstract(String name, String description, int price) {
+        super(name, description, price);
+    }
+
+    protected static final int CLASS_TYPE_ENTREE = 1;
+    protected static final int CLASS_TYPE_PLAT = 2;
+    protected static final int CLASS_TYPE_DESSERT = 3;
+    protected static final int CLASS_TYPE_DRINK = 4;
+
+    public static final Creator<ProductAbstract> CREATOR = new Creator<ProductAbstract>() {
+        @Override
+        public ProductAbstract createFromParcel(Parcel source) {
+            return ProductAbstract.getConcreteClass(source);
+        }
+
+        @Override
+        public ProductAbstract[] newArray(int size) {
+            return new ProductAbstract[size];
+        }
+    };
+
+    public static ProductAbstract getConcreteClass(Parcel source) {
+
+        switch (source.readInt()) {
+            case CLASS_TYPE_ENTREE:
+                return new Entree(source);
+            case CLASS_TYPE_PLAT:
+                return new Plat(source);
+            case CLASS_TYPE_DESSERT:
+                return new Dessert(source);
+            case CLASS_TYPE_DRINK:
+                return new Drink(source);
+            default:
+                return null;
+        }
+    }
+
+    protected ProductAbstract(Parcel in) {
+        super(in);
+    }
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        super.writeToParcel(dest, flags);
+
+    }
 
 
 }
