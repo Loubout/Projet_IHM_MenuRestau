166ef8dbdc4693a6fccbdaf5c660b4ecc29eb1d3
tom-ish <tomohiro.ishiwata@gmail.com>
2016-04-08 14:16:07 +0200

    onClickListener

--- Projet_IHM_android/app/src/main/java/com/example/willy/projet_ihm_android/fragment_view/LeftPaneFragment.java
+++ Projet_IHM_android/app/src/main/java/com/example/willy/projet_ihm_android/fragment_view/LeftPaneFragment.java
@@ -6,6 +6,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.willy.projet_ihm_android.R;
 
@@ -15,9 +17,12 @@ import com.example.willy.projet_ihm_android.R;
  */
 public class LeftPaneFragment extends ListFragment {
 
+    ListView listview;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.left_pane_fragment, container, false);
+        listview = (ListView) getActivity().findViewById(android.R.id.list);
         return view;
     }
 
@@ -30,6 +35,27 @@ public class LeftPaneFragment extends ListFragment {
                 R.layout.menu_item_layout, R.id.menuItem, items);
 
         setListAdapter(aa);
+    }
+
+    public View getViewByPosition(int pos, ListView listView) {
+        final int firstListItemPosition = listView.getFirstVisiblePosition();
+        final int lastListItemPosition = firstListItemPosition + listView.getChildCount() - 1;
+
+        if (pos < firstListItemPosition || pos > lastListItemPosition ) {
+            return listView.getAdapter().getView(pos, null, listView);
+        } else {
+            final int childIndex = pos - firstListItemPosition;
+            return listView.getChildAt(childIndex);
+        }
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int pos, long id) {
+        super.onListItemClick(l, v, pos, id);
+        View item = getViewByPosition(pos, l);
+        item.setSelected(true);
+
+        Toast.makeText(getActivity(), "Item " + pos + " was clicked", Toast.LENGTH_SHORT).show();
     }
 
 }
