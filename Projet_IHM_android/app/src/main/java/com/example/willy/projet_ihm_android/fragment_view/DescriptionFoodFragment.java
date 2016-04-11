80ef904ecbc14c5c3abd31ca52d1b414c7b73e31
Aonitit <willhuam@yahoo.fr>
2016-04-11 02:13:36 +0200

    test

--- Projet_IHM_android/app/src/main/java/com/example/willy/projet_ihm_android/fragment_view/DescriptionFoodFragment.java
+++ Projet_IHM_android/app/src/main/java/com/example/willy/projet_ihm_android/fragment_view/DescriptionFoodFragment.java
@@ -6,11 +6,13 @@ package com.example.willy.projet_ihm_android.fragment_view;
  */
 
 //=======
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.willy.projet_ihm_android.R;
@@ -23,15 +25,17 @@ public class DescriptionFoodFragment extends PaneFragment {
     private TextView food_desc;
     private TextView food_prix;
     private Button buttonAddPanier;
+    private ImageView food_img;
 
     private String name;
     private String desc;
     private String prix;
-
-    public DescriptionFoodFragment(String n, String d, String p) {
+    private Drawable img;
+    public DescriptionFoodFragment(String n, String d, String p, Drawable i) {
         name = n;
         desc = d;
         prix = p;
+        img=i;
     }
 
 
@@ -40,16 +44,16 @@ public class DescriptionFoodFragment extends PaneFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view= inflater.inflate(R.layout.description_food, container, false);
-
+        food_img = (ImageView) view.findViewById(R.id.food_img);
         food_name = (TextView) view.findViewById(R.id.food_name);
         food_desc= (TextView) view.findViewById(R.id.food_desc);
         food_prix= (TextView) view.findViewById(R.id.food_prix);
         buttonAddPanier = (Button) view.findViewById(R.id.buttonAddPanier);
 
-        food_name.setText(name);
-        food_desc.setText(desc);
-        food_prix.setText(prix);
-
+        food_name.setText("NOM DU PLAT :\n"+name);
+        food_desc.setText("\nDESCRIPTION :\n"+desc);
+        food_prix.setText("\nPRIX :\n"+prix);
+        food_img.setImageDrawable(img);
         return view;
     }
 
