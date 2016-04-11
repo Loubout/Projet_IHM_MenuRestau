723bc032d0041a44d48c66a809ea43ca3ddad4c9
tom-ish <tomohiro.ishiwata@gmail.com>
2016-04-11 19:21:39 +0200

    tomo merge

--- Projet_IHM_android/app/src/main/java/com/example/willy/projet_ihm_android/MainActivity.java
+++ Projet_IHM_android/app/src/main/java/com/example/willy/projet_ihm_android/MainActivity.java
@@ -9,8 +9,12 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.willy.projet_ihm_android.data.food.ItemAbstract;
 import com.example.willy.projet_ihm_android.fragment_view.AperitifFragment;
 import com.example.willy.projet_ihm_android.fragment_view.BoissonFragment;
 import com.example.willy.projet_ihm_android.fragment_view.CarrousselFragment;
@@ -22,6 +26,8 @@ import com.example.willy.projet_ihm_android.fragment_view.PaneFragment;
 import com.example.willy.projet_ihm_android.fragment_view.PlatFragment;
 import com.example.willy.projet_ihm_android.panier.PanierFragment;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity implements LeftPaneFragment.OnArticleSelectedListener {
 
     FragmentManager manager;
@@ -29,10 +35,15 @@ public class MainActivity extends AppCompatActivity implements LeftPaneFragment.
 
     PaneFragment currentMainFragment;
 
+    ArrayList<ItemAbstract> panier;
+
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("", "MainActivity started");
+
+
+        panier = new ArrayList<ItemAbstract>();
 
         LeftPaneFragment leftMenuFragment = new LeftPaneFragment();
         CarrousselFragment carrousselFragment = new CarrousselFragment();
@@ -40,7 +51,46 @@ public class MainActivity extends AppCompatActivity implements LeftPaneFragment.
         Toolbar mToolBar = (Toolbar) findViewById(R.id.topPaneToolbar);
         setSupportActionBar(mToolBar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
+        //getSupportActionBar().setIcon(R.drawable.logo_icon);
+        TextView toolbartitle = (TextView) findViewById(R.id.titleActionBar);
+        toolbartitle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                FragmentManager fragmentManager = getFragmentManager();
+                transaction = fragmentManager.beginTransaction();
+                transaction.replace(R.id.mainPanel, new CarrousselFragment());
+                transaction.addToBackStack(null);
+                transaction.commit();
+                Toast.makeText(MainActivity.this, "Title toolBar clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        ImageView logoIcon = (ImageView) findViewById(R.id.logoIcon);
+        logoIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                FragmentManager fragmentManager = getFragmentManager();
+                transaction = fragmentManager.beginTransaction();
+                transaction.replace(R.id.mainPanel, new CarrousselFragment());
+                transaction.addToBackStack(null);
+                transaction.commit();
+                Toast.makeText(MainActivity.this, "logoIcon click", Toast.LENGTH_SHORT).show();
+            }
+        });
+/*
+        Field titleField = Toolbar.class.getDeclaredField("mTitleTextView");
+        titleField.setAccessible(true);
+        TextView barTitleView = (TextView) titleField.get(mToolBar);
+        barTitleView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(MainActivity.this, "HELLO WORLD", Toast.LENGTH_SHORT).show();
+            }
+        });
+*/
         currentMainFragment = carrousselFragment;
 
         manager = getFragmentManager();
@@ -56,20 +106,26 @@ public class MainActivity extends AppCompatActivity implements LeftPaneFragment.
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.top_pane_toolbar, menu);
+        //final int titleId = getResources().getIdentifier("logo_icon", "drawable", MainActivity.this.getPackageName());
+
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        FragmentManager fragmentManager;
         switch (item.getItemId()) {
             case R.id.action_panier:
                 // User chose the "Settings" item, show the app settings UI...
-                FragmentManager fragmentManager = getFragmentManager();
-                FragmentTransaction transaction = fragmentManager.beginTransaction();
+                fragmentManager = getFragmentManager();
+                transaction = fragmentManager.beginTransaction();
                 transaction.replace(R.id.mainPanel, new PanierFragment());
                 transaction.addToBackStack(null);
                 transaction.commit();
-                Toast.makeText(MainActivity.this, "PanierClick ", Toast.LENGTH_SHORT).show();return true;
+                Toast.makeText(MainActivity.this, "PanierClick ", Toast.LENGTH_SHORT).show();
+                return true;
+
+
 
             default:
                 // If we got here, the user's action was not recognized.
