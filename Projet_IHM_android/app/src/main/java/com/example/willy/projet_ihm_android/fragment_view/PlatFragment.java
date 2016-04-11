ff4c04283bc2ee56b272fab58746a17945f30e28
Aonitit <willhuam@yahoo.fr>
2016-04-11 17:16:54 +0200

    will

--- Projet_IHM_android/app/src/main/java/com/example/willy/projet_ihm_android/fragment_view/PlatFragment.java
+++ Projet_IHM_android/app/src/main/java/com/example/willy/projet_ihm_android/fragment_view/PlatFragment.java
@@ -2,6 +2,7 @@ package com.example.willy.projet_ihm_android.fragment_view;
 
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,7 +17,7 @@ import com.origamilabs.library.views.StaggeredGridView;
  */
 public class PlatFragment extends PaneFragment {
     MainActivity act ;
-
+    int cpt=0;
     StaggeredGridView platView;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -24,93 +25,121 @@ public class PlatFragment extends PaneFragment {
         platView = (StaggeredGridView) view.findViewById(R.id.gridview);
         platView.setAdapter(ImageAdapter.getDishesImageAdapter(getActivity()));
         platView.setOnItemClickListener(new StaggeredGridView.OnItemClickListener() {
-            public void onItemClick(StaggeredGridView parent, View v,
-                                    int position, long id) {
-              //  Toast.makeText(getActivity(), "" + position,
-               //         Toast.LENGTH_SHORT).show();
+            public void onItemClick(StaggeredGridView parento, View vo,
+                                    int positiono, long id) {
+                //  Toast.makeText(getActivity(), "" + position,
+                //         Toast.LENGTH_SHORT).show();
+                final StaggeredGridView parent = parento;
+                final View v = vo;
+                final int position = positiono;
+                cpt++;
+                Handler handler = new Handler();
+                Runnable r = new Runnable() {
+                    @Override
+                    public void run() {
+                        if (cpt == 1) {
+                            System.out.println("ClickSIMPLE///////////////////////////////////");
+                            cpt = 0;
+
+                            String itemId = ((ImageAdapter.Item) (parent.getAdapter().getItem(position))).name;
+                            Drawable i = null;
+                            String n = null;
+                            String d = null;
+                            String p = null;
+                            switch (itemId) {
+                                case "item14":
+                                    i = getActivity().getResources().getDrawable(R.drawable.food_plat_item14);
+                                    break;
+                                case "item15":
+                                    i = getActivity().getResources().getDrawable(R.drawable.food_plat_item15);
+                                    break;
+                                case "item16":
+                                    i = getActivity().getResources().getDrawable(R.drawable.food_plat_item16);
+                                    break;
+                                case "item17":
+                                    i = getActivity().getResources().getDrawable(R.drawable.food_plat_item17);
+                                    break;
+                                case "item18":
+                                    i = getActivity().getResources().getDrawable(R.drawable.food_plat_item18);
+                                    break;
+                            }
+
+                            switch (itemId) {
+                                case "item14":
+                                    n = getActivity().getResources().getString(R.string.item14_name);
+                                    break;
+                                case "item15":
+                                    n = getActivity().getResources().getString(R.string.item15_name);
+                                    break;
+                                case "item16":
+                                    n = getActivity().getResources().getString(R.string.item16_name);
+                                    break;
+                                case "item17":
+                                    n = getActivity().getResources().getString(R.string.item17_name);
+                                    break;
+                                case "item18":
+                                    n = getActivity().getResources().getString(R.string.item18_name);
+                                    break;
+                            }
+
+                            switch (itemId) {
+                                case "item14":
+                                    d = getActivity().getResources().getString(R.string.item14_desc);
+                                    break;
+                                case "item15":
+                                    d = getActivity().getResources().getString(R.string.item15_desc);
+                                    break;
+                                case "item16":
+                                    d = getActivity().getResources().getString(R.string.item16_desc);
+                                    break;
+                                case "item17":
+                                    d = getActivity().getResources().getString(R.string.item17_desc);
+                                    break;
+                                case "item18":
+                                    d = getActivity().getResources().getString(R.string.item18_desc);
+                                    break;
+                            }
+
+                            switch (itemId) {
+                                case "item14":
+                                    p = getActivity().getResources().getString(R.string.item14_price);
+                                    break;
+                                case "item15":
+                                    p = getActivity().getResources().getString(R.string.item15_price);
+                                    break;
+                                case "item16":
+                                    p = getActivity().getResources().getString(R.string.item16_price);
+                                    break;
+                                case "item17":
+                                    p = getActivity().getResources().getString(R.string.item17_price);
+                                    break;
+                                case "item18":
+                                    p = getActivity().getResources().getString(R.string.item18_price);
+                                    break;
+                            }
 
 
-                String itemId = ((ImageAdapter.Item)(parent.getAdapter().getItem(position))).name;
-                Drawable i=null;
-                String  n=null;
-                String d = null;
-                String p = null;
-                switch (itemId) {
-                    case "item14":
-                        i=getActivity().getResources().getDrawable(R.drawable.food_plat_item14);
-                        break;
-                    case "item15":
-                        i=getActivity().getResources().getDrawable(R.drawable.food_plat_item15);
-                        break;
-                    case "item16":
-                        i=getActivity().getResources().getDrawable(R.drawable.food_plat_item16);
-                        break;
-                    case "item17":
-                        i=getActivity().getResources().getDrawable(R.drawable.food_plat_item17);
-                        break;
-                    case "item18":
-                        i=getActivity().getResources().getDrawable(R.drawable.food_plat_item18);
-                        break;
+                            act = (MainActivity) getActivity();
+                            act.afficheDesc(n, d, p, i);
+                        }
+                    }
+                };
+                if (cpt == 1) {
+
+                    handler.postDelayed(r, 300);
+                } else if (cpt == 2) {
+                    //Double click
+                    cpt = 0;
+                    System.out.println("/////////////////////////////DOUBLE CLICK/////////");
                 }
 
-                switch (itemId) {
-                    case "item14":
-                        n=getActivity().getResources().getString(R.string.item14_name);
-                        break;
-                    case "item15":
-                        n=getActivity().getResources().getString(R.string.item15_name);
-                        break;
-                    case "item16":
-                        n=getActivity().getResources().getString(R.string.item16_name);
-                        break;
-                    case "item17":
-                        n=getActivity().getResources().getString(R.string.item17_name);
-                        break;
-                    case "item18":
-                        n=getActivity().getResources().getString(R.string.item18_name);
-                        break;
-                }
-
-                switch (itemId) {
-                    case "item14":
-                        d=getActivity().getResources().getString(R.string.item14_desc);
-                        break;
-                    case "item15":
-                        d=getActivity().getResources().getString(R.string.item15_desc);
-                        break;
-                    case "item16":
-                        d=getActivity().getResources().getString(R.string.item16_desc);
-                        break;
-                    case "item17":
-                        d=getActivity().getResources().getString(R.string.item17_desc);
-                        break;
-                    case "item18":
-                        d=getActivity().getResources().getString(R.string.item18_desc);
-                        break;
-                }
-
-                switch (itemId) {
-                    case "item14":
-                        p=getActivity().getResources().getString(R.string.item14_price);
-                        break;
-                    case "item15":
-                        p=getActivity().getResources().getString(R.string.item15_price);
-                        break;
-                    case "item16":
-                        p=getActivity().getResources().getString(R.string.item16_price);
-                        break;
-                    case "item17":
-                        p=getActivity().getResources().getString(R.string.item17_price);
-                        break;
-                    case "item18":
-                        p=getActivity().getResources().getString(R.string.item18_price);
-                        break;
-                }
 
 
 
-                act = (MainActivity) getActivity();
-                act.afficheDesc(n, d, p, i);
+/*
+                    // ça c'est l'id de l'item tu peux le modifier dans les constructeurs de ImageAdapter
+
+*/
 
 
             }
