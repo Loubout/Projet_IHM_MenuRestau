20dfd8d5cb3b850b7077412a62fe3abfe9a4204d
tom-ish <tomohiro.ishiwata@gmail.com>
2016-04-16 00:20:52 +0200

    integrating double fragments

---
+++ Projet_IHM_android/app/src/main/java/com/example/willy/projet_ihm_android/fragment_view/MainBottomFragment.java
@@ -0,0 +1,9 @@
+package com.example.willy.projet_ihm_android.fragment_view;
+
+import android.app.Fragment;
+
+/**
+ * Created by Tomohiro on 14/04/16.
+ */
+public class MainBottomFragment extends Fragment {
+}
